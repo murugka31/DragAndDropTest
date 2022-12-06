bea911af0dccf8234f82223006d8010ec722dc36
Елена <murugka31@gmail.com>
2022-12-06 09:31:21 +0300

    added Configuration.baseUrl

--- src/test/java/com/emurugova/RectangleDragAndDropTest.java
+++ src/test/java/com/emurugova/RectangleDragAndDropTest.java
@@ -1,25 +1,23 @@
 package com.emurugova;
 
-
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-
 import static com.codeborne.selenide.Condition.exactTextCaseSensitive;
 import static com.codeborne.selenide.Selenide.*;
-
 
 public class RectangleDragAndDropTest {
 
     @BeforeAll
     static void beforeAll() {
         Configuration.browserSize = "2100x1080";
+        Configuration.baseUrl = "https://the-internet.herokuapp.com";
     }
 
     @Test
     void dragAndDropAction(){
-      open("https://the-internet.herokuapp.com/drag_and_drop");
+       open("/drag_and_drop");
        $("#column-a").shouldHave(exactTextCaseSensitive("A"));
        $("#column-a").dragAndDropTo("#column-b");
        $("#column-a").shouldHave(exactTextCaseSensitive("B"));
